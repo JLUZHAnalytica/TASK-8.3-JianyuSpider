db559606d6d7badbf52d7ee1da4d74e538acfdf2
Yokey <zyf6@outlook.com>
2020-08-04 10:51:04 +0800

    优化时间匹配

--- zyf/parse_bidtime.py
+++ zyf/parse_bidtime.py
@@ -3,10 +3,11 @@ import re
 
 def parse(text):
     bidtime_place = []
-    bidtime_list = re.findall(r"([0-9]+年[0-9]+月[0-9]+日\s*.*?)[(（]*北京", text)
+    bidtime_list = re.findall(
+        r"([0-9]+年[0-9]+月[0-9]+日\s*[0-9]+[:点][0-9]+[分]*)", text)
     if len(bidtime_list) == 0:
         return
-    for m in re.finditer(r"([0-9]+年[0-9]+月[0-9]+日\s*.*?)[(（]*北京", text):
+    for m in re.finditer(r"([0-9]+年[0-9]+月[0-9]+日\s*[0-9]+[:点][0-9]+[分]*)", text):
         bidtime_place.append(m.span()[0])
     title_place = re.search(r"开标时间", text).span()[0]
     if title_place == None:
